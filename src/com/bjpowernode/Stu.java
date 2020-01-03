fab37ae2f1b3e61be3aa954bb48eb93d3c15890a
bjpowernodewyz <1107162080@qq.com>
2020-01-03 20:35:29 +0800

    添加了getter and setter

--- src/com/bjpowernode/Stu.java
+++ src/com/bjpowernode/Stu.java
@@ -11,4 +11,28 @@ public class Stu {
     private String addr;
     private String master;
     private String bran02;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
