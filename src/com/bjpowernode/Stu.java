001fc154d4446214a48be562b0edaa620cf0d39c
bjpowernodewyz <1107162080@qq.com>
2020-01-03 20:36:24 +0800

    添加了getter and setter

--- src/com/bjpowernode/Stu.java
+++ src/com/bjpowernode/Stu.java
@@ -11,4 +11,28 @@ public class Stu {
     private String addr;
     private String master;
     private String bran02;
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getMaster() {
+        return master;
+    }
+
+    public void setMaster(String master) {
+        this.master = master;
+    }
+
+    public String getBran02() {
+        return bran02;
+    }
+
+    public void setBran02(String bran02) {
+        this.bran02 = bran02;
+    }
 }
